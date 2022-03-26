d413d222f6c7cf672ec8ec300f9d6583f33b4946
ogata <ogata@beaglee.com>
2022-03-27 02:52:31 +0900

    不要な記述を削除

--- src/nextjs/pages/index.js
+++ src/nextjs/pages/index.js
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import packageJson from "../package.json";
 
 function Content(props) {
   const liff = props.liff;
@@ -7,6 +6,10 @@ function Content(props) {
 
   if (liffError) {
     return <div>{liffError}</div>;
+  }
+
+  if (!liff) {
+    return <div>liff is undefined...</div>;
   }
 
   if (!liff.isInClient()) {
@@ -25,47 +28,6 @@ function Content(props) {
           LIFF Starter!
         </a>
       </h1>
-      <div className="home__badges">
-        <span className="home__badges__badge badge--primary">LIFF Starter</span>
-        <span className="home__badges__badge badge--secondary">nextjs</span>
-        <span className="home__badges__badge badge--primary">
-          {packageJson.version}
-        </span>
-        <a
-          href="https://github.com/line/line-liff-v2-starter"
-          target="_blank"
-          rel="noreferrer"
-          className="home__badges__badge badge--secondary"
-        >
-          GitHub
-        </a>
-      </div>
-      <div className="home__buttons">
-        <a
-          href="https://developers.line.biz/en/docs/liff/developing-liff-apps/"
-          target="_blank"
-          rel="noreferrer"
-          className="home__buttons__button button--primary"
-        >
-          LIFF Documentation
-        </a>
-        <a
-          href="https://liff-playground.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-          className="home__buttons__button button--tertiary"
-        >
-          LIFF Playground
-        </a>
-        <a
-          href="https://developers.line.biz/console/"
-          target="_blank"
-          rel="noreferrer"
-          className="home__buttons__button button--secondary"
-        >
-          LINE Developers Console
-        </a>
-      </div>
     </>
   );
 }
