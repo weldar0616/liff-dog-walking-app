a2c7fa7179df55cd1b5438292b530c730da92423
ogata <ogata@beaglee.com>
2022-03-26 23:28:35 +0900

    未認証の場合コンテンツを表示しないように #1

--- src/nextjs/pages/index.js
+++ src/nextjs/pages/index.js
@@ -1,6 +1,71 @@
 import Head from "next/head";
 import packageJson from "../package.json";
 
+function Content() {
+  let content = <div>Unautorized...</div>;
+  import("@line/liff").then((liff) => {
+    if (liff.isLoggedIn) {
+      content = (
+        <>
+          <h1 className="home__title">
+            Welcome to <br />
+            <a
+              className="home__title__link"
+              href="https://developers.line.biz/en/docs/liff/overview/"
+            >
+              LIFF Starter!
+            </a>
+          </h1>
+          <div className="home__badges">
+            <span className="home__badges__badge badge--primary">
+              LIFF Starter
+            </span>
+            <span className="home__badges__badge badge--secondary">nextjs</span>
+            <span className="home__badges__badge badge--primary">
+              {packageJson.version}
+            </span>
+            <a
+              href="https://github.com/line/line-liff-v2-starter"
+              target="_blank"
+              rel="noreferrer"
+              className="home__badges__badge badge--secondary"
+            >
+              GitHub
+            </a>
+          </div>
+          <div className="home__buttons">
+            <a
+              href="https://developers.line.biz/en/docs/liff/developing-liff-apps/"
+              target="_blank"
+              rel="noreferrer"
+              className="home__buttons__button button--primary"
+            >
+              LIFF Documentation
+            </a>
+            <a
+              href="https://liff-playground.netlify.app/"
+              target="_blank"
+              rel="noreferrer"
+              className="home__buttons__button button--tertiary"
+            >
+              LIFF Playground
+            </a>
+            <a
+              href="https://developers.line.biz/console/"
+              target="_blank"
+              rel="noreferrer"
+              className="home__buttons__button button--secondary"
+            >
+              LINE Developers Console
+            </a>
+          </div>
+        </>
+      );
+    }
+  });
+  return content;
+}
+
 export default function Home(props) {
   /** You can access to liff and liffError object through the props.
    *  const { liff, liffError } = props;
@@ -14,59 +79,9 @@ export default function Home(props) {
         <title>散歩当番</title>
       </Head>
       <div className="home">
-        <h1 className="home__title">
-          Welcome to <br />
-          <a
-            className="home__title__link"
-            href="https://developers.line.biz/en/docs/liff/overview/"
-          >
-            LIFF Starter!
-          </a>
-        </h1>
-        <div className="home__badges">
-          <span className="home__badges__badge badge--primary">
-            LIFF Starter
-          </span>
-          <span className="home__badges__badge badge--secondary">nextjs</span>
-          <span className="home__badges__badge badge--primary">
-            {packageJson.version}
-          </span>
-          <a
-            href="https://github.com/line/line-liff-v2-starter"
-            target="_blank"
-            rel="noreferrer"
-            className="home__badges__badge badge--secondary"
-          >
-            GitHub
-          </a>
-        </div>
-        <div className="home__buttons">
-          <a
-            href="https://developers.line.biz/en/docs/liff/developing-liff-apps/"
-            target="_blank"
-            rel="noreferrer"
-            className="home__buttons__button button--primary"
-          >
-            LIFF Documentation
-          </a>
-          <a
-            href="https://liff-playground.netlify.app/"
-            target="_blank"
-            rel="noreferrer"
-            className="home__buttons__button button--tertiary"
-          >
-            LIFF Playground
-          </a>
-          <a
-            href="https://developers.line.biz/console/"
-            target="_blank"
-            rel="noreferrer"
-            className="home__buttons__button button--secondary"
-          >
-            LINE Developers Console
-          </a>
-        </div>
+        <Content />
       </div>
     </div>
   );
+  // return <div>Hoge...</div>;
 }
