984bd50a95acca75bacc09221fc6b3248d13e57c
ogata <ogata@beaglee.com>
2022-03-27 03:08:14 +0900

    当番表確認、散歩報告、設定変更ページを追加

---
+++ src/nextjs/pages/report.js
@@ -0,0 +1,8 @@
+export default function Report() {
+  return (
+    <>
+      <h2>報告画面</h2>
+      <div>開発中...</div>
+    </>
+  );
+}
---
+++ src/nextjs/pages/settings.js
@@ -0,0 +1,8 @@
+export default function Settings() {
+  return (
+    <>
+      <h2>設定画面</h2>
+      <div>開発中...</div>
+    </>
+  );
+}
