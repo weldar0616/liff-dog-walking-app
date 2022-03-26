920040b1d5ce68c396f61ab36d99772c4ad27e47
ogata <ogata@beaglee.com>
2022-03-27 01:30:36 +0900

    認証を撤廃

--- src/nextjs/pages/index.js
+++ src/nextjs/pages/index.js
@@ -1,84 +1,71 @@
 import Head from "next/head";
 import packageJson from "../package.json";
 
-function Content(props) {
-  const { liff } = props;
-
-  if (liff?.isLoggedIn()) {
-    return (
-      <>
-        <h1 className="home__title">
-          Welcome to <br />
-          <a
-            className="home__title__link"
-            href="https://developers.line.biz/en/docs/liff/overview/"
-          >
-            LIFF Starter!
-          </a>
-        </h1>
-        <div className="home__badges">
-          <span className="home__badges__badge badge--primary">
-            LIFF Starter
-          </span>
-          <span className="home__badges__badge badge--secondary">nextjs</span>
-          <span className="home__badges__badge badge--primary">
-            {packageJson.version}
-          </span>
-          <a
-            href="https://github.com/line/line-liff-v2-starter"
-            target="_blank"
-            rel="noreferrer"
-            className="home__badges__badge badge--secondary"
-          >
-            GitHub
-          </a>
-        </div>
-        <div className="home__buttons">
-          <a
-            href="https://developers.line.biz/en/docs/liff/developing-liff-apps/"
-            target="_blank"
-            rel="noreferrer"
-            className="home__buttons__button button--primary"
-          >
-            LIFF Documentation
-          </a>
-          <a
-            href="https://liff-playground.netlify.app/"
-            target="_blank"
-            rel="noreferrer"
-            className="home__buttons__button button--tertiary"
-          >
-            LIFF Playground
-          </a>
-          <a
-            href="https://developers.line.biz/console/"
-            target="_blank"
-            rel="noreferrer"
-            className="home__buttons__button button--secondary"
-          >
-            LINE Developers Console
-          </a>
-        </div>
-      </>
-    );
-  }
-  return <div>Unauthorized...</div>;
+function Content() {
+  return (
+    <>
+      <h1 className="home__title">
+        Welcome to <br />
+        <a
+          className="home__title__link"
+          href="https://developers.line.biz/en/docs/liff/overview/"
+        >
+          LIFF Starter!
+        </a>
+      </h1>
+      <div className="home__badges">
+        <span className="home__badges__badge badge--primary">LIFF Starter</span>
+        <span className="home__badges__badge badge--secondary">nextjs</span>
+        <span className="home__badges__badge badge--primary">
+          {packageJson.version}
+        </span>
+        <a
+          href="https://github.com/line/line-liff-v2-starter"
+          target="_blank"
+          rel="noreferrer"
+          className="home__badges__badge badge--secondary"
+        >
+          GitHub
+        </a>
+      </div>
+      <div className="home__buttons">
+        <a
+          href="https://developers.line.biz/en/docs/liff/developing-liff-apps/"
+          target="_blank"
+          rel="noreferrer"
+          className="home__buttons__button button--primary"
+        >
+          LIFF Documentation
+        </a>
+        <a
+          href="https://liff-playground.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+          className="home__buttons__button button--tertiary"
+        >
+          LIFF Playground
+        </a>
+        <a
+          href="https://developers.line.biz/console/"
+          target="_blank"
+          rel="noreferrer"
+          className="home__buttons__button button--secondary"
+        >
+          LINE Developers Console
+        </a>
+      </div>
+    </>
+  );
 }
 
 export default function Home(props) {
-  /** You can access to liff and liffError object through the props.
-   *  const { liff, liffError } = props;
-   *  console.log(liff.getVersion());
-   *
-   *  Learn more about LIFF API documentation (https://developers.line.biz/en/reference/liff)
-   **/
   return (
     <div>
       <Head>
         <title>散歩当番</title>
       </Head>
       <div className="home">
-        <Content liff={props.liff} />
+        <Content />
       </div>
     </div>
   );
