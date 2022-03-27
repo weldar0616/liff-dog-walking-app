95d24bc57d44bb744d8426c3aa45b49f68e4f8c7
ogata <ogata@beaglee.com>
2022-03-27 12:46:23 +0900

    liffSDKをCDNに変更

--- src/nextjs/next.config.js
+++ src/nextjs/next.config.js
@@ -3,4 +3,8 @@ module.exports = {
   env: {
     LIFF_ID: process.env.LIFF_ID,
   },
+  head: {
+    // inspired by https://mamunga.com/nuxt-liff-init.
+    script: [{ src: "https://static.line-scdn.net/liff/edge/2/sdk.js" }],
+  },
 };
