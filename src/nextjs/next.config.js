8c750d1f2c0b5f5388e47434405b3051383069d8
ogata <ogata@beaglee.com>
2022-03-27 13:12:09 +0900

    liffSDKをnpm経由に戻した

--- src/nextjs/next.config.js
+++ src/nextjs/next.config.js
@@ -3,8 +3,4 @@ module.exports = {
   env: {
     LIFF_ID: process.env.LIFF_ID,
   },
-  head: {
-    // inspired by https://mamunga.com/nuxt-liff-init.
-    script: [{ src: "https://static.line-scdn.net/liff/edge/2/sdk.js" }],
-  },
 };
