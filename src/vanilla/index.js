084a03abcd88206543854c81bb2d47ba244e9e8f
ogata <ogata@beaglee.com>
2022-03-27 14:00:41 +0900

    liff.sendMessage検証

--- src/vanilla/index.js
+++ src/vanilla/index.js
@@ -2,18 +2,28 @@ import "./index.css";
 import liff from "@line/liff";
 
 document.addEventListener("DOMContentLoaded", function () {
+  alert(JSON.stringify(liff));
   liff
     .init({ liffId: process.env.LIFF_ID })
     .then(() => {
       console.log("Success! you can do something with LIFF API here.");
-      liff.sendMessages([
-        {
-          type: "text",
-          text: "Hello:" + JSON.stringify({}),
-        },
-      ]);
+      alert("Success! you can do something with LIFF API here.");
+      liff
+        .sendMessages([
+          {
+            type: "text",
+            text: "Hello:" + JSON.stringify({}),
+          },
+        ])
+        .then((res) => {
+          alert(JSON.stringify(res));
+        })
+        .catch((err) => {
+          alert(JSON.stringify(err));
+        });
     })
     .catch((error) => {
       console.log(error);
+      alert(JSON.stringify(error));
     });
 });
