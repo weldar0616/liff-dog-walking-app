b546062112566a7881aae2c4054e12e3c14e6047
ogata <ogata@beaglee.com>
2022-03-27 18:23:27 +0900

    liffSDK試行

--- src/nextjs/pages/report.js
+++ src/nextjs/pages/report.js
@@ -1,28 +1,30 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Report(props) {
+  const [liffObject, setLiffObject] = useState(null);
+
   useEffect(async () => {
     const liff = (await import("@line/liff")).default;
     await liff.init({
       liffId: process.env.LIFF_ID_REPORT_APP,
     });
+    setLiffObject(liff);
+    const profile = await liff.getProfile();
     await liff
-    .sendMessages([
-      {
-        type: "text",
-        text: JSON.stringify(profile),
-      },
-    ])
-    .then(() => {
-      liff.closeWindow();
-    })
-    .catch((error) => {
-      alert(JSON.stringify(error));
-    });
+      .sendMessages([
+        {
+          type: "text",
+          text: JSON.stringify(profile),
+        },
+      ])
+      .then(() => {
+        liff.closeWindow();
+      })
+      .catch((error) => {
+        alert(JSON.stringify(error));
+      });
   });
 
   // TODO: APIのエンドポイントとして使いたい時 Next.jsではどうする?
-  return (
-    <></>
-  );
+  return <></>;
 }
