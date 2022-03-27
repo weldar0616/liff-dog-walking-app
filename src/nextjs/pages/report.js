54a2a6140f95047d06f36f3216471f143bdf35d5
ogata <ogata@beaglee.com>
2022-03-27 12:40:29 +0900

    liff.sendMessages 検証コミット

--- src/nextjs/pages/report.js
+++ src/nextjs/pages/report.js
@@ -1,22 +1,13 @@
 import { Button } from "@mui/material";
 
 export default function Report(props) {
+  console.log({props});
   const liff = props.liff;
   const liffError = props.liffError;
 
-  if (liffError) {
-    return <div>{liffError}</div>;
-  }
-
-  if (!liff) {
-    return <div>liff is undefined...</div>;
-  }
-
-  console.log({ liff });
-
-  // if (props.liff && props.liff.isLoggedIn()) {
-  try {
-    alert("sendMessages", JSON.stringify(props.liff));
+  const XXX = (props) => {
+    console.log("sendMessages", JSON.stringify(props.liff));
+    const liff = props.liff;
     liff.getProfile().then((profile) => {
       console.log({ profile });
       liff
@@ -27,32 +18,37 @@ export default function Report(props) {
           },
         ])
         .then((res) => {
-          window.alert("closeWindow", JSON.stringify(res));
+          console.log("closeWindow", JSON.stringify(res));
           liff.closeWindow();
         })
         .catch((err) => {
-          window.alert("reject send messages: " + JSON.stringify(err));
+          console.log("reject send messages: " + JSON.stringify(err));
           liff.closeWindow();
         });
     });
-  } catch (err) {
-    return <div>{err}</div>;
-  }
-  return <div>Loading...</div>;
-  // }
-
-  const onClick = () => {
-    if (props.liff && !props.liff.isLoggedIn()) {
-      props.liff.login();
-    }
+    return <span>Loading...</span>;
   };
-  return (
-    <>
-      <div>
-        <Button variant="outlined" onClick={onClick}>
-          Login
-        </Button>
-      </div>
-    </>
+
+  console.log({ liff });
+  const content = (
+    <div>
+      {liffError ? liffError : !liff ? "liff is undefined..." : <XXX liff={liff} />}
+    </div>
   );
+
+  return content;
+  // const onClick = () => {
+  //   if (props.liff && !props.liff.isLoggedIn()) {
+  //     props.liff.login();
+  //   }
+  // };
+  // return (
+  //   <>
+  //     <div>
+  //       <Button variant="outlined" onClick={onClick}>
+  //         Login
+  //       </Button>
+  //     </div>
+  //   </>
+  // );
 }
