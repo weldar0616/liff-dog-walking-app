41f0ca00720c0a1c81bdc59816656f1abf9b5cda
ogata <ogata@beaglee.com>
2022-03-27 13:24:13 +0900

    liff.sendMessages 検証コミット

--- src/nextjs/pages/report.js
+++ src/nextjs/pages/report.js
@@ -1,38 +1,48 @@
 import { Button } from "@mui/material";
 
 export default function Report(props) {
-  console.log({props});
+  console.log({ props });
   const liff = props.liff;
   const liffError = props.liffError;
 
   const XXX = (props) => {
-   alert("sendMessages:" + JSON.stringify(props.liff));
+    alert("sendMessages:" + JSON.stringify(props.liff));
     const liff = props.liff;
-    liff.getProfile().then((profile) => {
-      console.log({ profile });
-      liff
-        .sendMessages([
-          {
-            type: "text",
-            text: "Hello:" + JSONs.stringify(profile),
-          },
-        ])
-        .then((res) => {
-          alert("closeWindow:"+ JSON.stringify(res));
-          liff.closeWindow();
-        })
-        .catch((err) => {
-          alert("reject send messages: " + JSON.stringify(err));
-          liff.closeWindow();
-        });
-    });
+    try {
+      liff.getProfile().then((profile) => {
+        console.log({ profile });
+        liff
+          .sendMessages([
+            {
+              type: "text",
+              text: "Hello:" + JSONs.stringify(profile),
+            },
+          ])
+          .then((res) => {
+            alert("closeWindow:" + JSON.stringify(res));
+            liff.closeWindow();
+          })
+          .catch((err) => {
+            alert("reject send messages: " + JSON.stringify(err));
+            liff.closeWindow();
+          });
+      });
+    } catch (err) {
+      return <span>{err}</span>;
+    }
     return <span>Loading...</span>;
   };
 
   console.log({ liff });
   const content = (
     <div>
-      {liffError ? liffError : !liff ? "liff is undefined..." : <XXX liff={liff} />}
+      {liffError ? (
+        liffError
+      ) : !liff ? (
+        "liff is undefined..."
+      ) : (
+        <XXX liff={liff} />
+      )}
     </div>
   );
 
