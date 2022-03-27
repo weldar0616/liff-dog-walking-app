57d2cb5cb7d30ecbbeadfad8baee87633c2d7fb9
ogata <ogata@beaglee.com>
2022-03-27 18:19:13 +0900

    liffSDK試行

--- src/nextjs/pages/_app.js
+++ src/nextjs/pages/_app.js
@@ -1,66 +1,35 @@
 import "../styles/globals.css";
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
-  const [liffObject, setLiffObject] = useState(null);
+  // const [liffObject, setLiffObject] = useState(null);
   // const [liffError, setLiffError] = useState(null);
-  const [liffProfile, setLiffProfile] = useState({
-    userId: "",
-    displayName: "",
-    pictureUrl: "",
-    statusMessage: "",
-  });
 
   // Execute liff.init() when the app is initialized
-  useEffect(async () => {
-    // to avoid `window is not defined` error
-    const liff = (await import("@line/liff")).default;
-    try {
-      console.log("start liff.init()...");
-      // alert("start liff.init()...");
-      await liff.init({
-        liffId: process.env.LIFF_ID,
-      });
-      console.log("liff.init() done");
-      // alert("liff.init() done");
-      setLiffObject(liff);
-      console.log("useState", { liff });
-
-      // FIXME: ここで定義するとほかのコンポーネントにも影響がある
-
-      const profile = await liff.getProfile();
-      setLiffProfile(profile);
-      console.log("useState", { profile });
-
-      await liff
-        .sendMessages([
-          {
-            type: "text",
-            text: JSON.stringify(profile),
-          },
-        ])
-        .then(() => {
-          alert("sendMessages done! -> close window!");
-          liff.closeWindow();
-        })
-        .catch((error) => {
-          alert(JSON.stringify(error));
-        });
-    } catch (error) {
-      console.log(`liff.init() failed: ${error}`);
-      if (!process.env.liffId) {
-        console.info(
-          "LIFF Starter: Please make sure that you provided `LIFF_ID` as an environmental variable."
-        );
-      }
-      // setLiffError(error.toString());
-    }
-  }, []);
+  // useEffect(async () => {
+  //   // to avoid `window is not defined` error
+  //   const liff = (await import("@line/liff")).default;
+  //   try {
+  //     console.log("start liff.init()...");
+  //     await liff.init({
+  //       liffId: process.env.LIFF_ID,
+  //     });
+  //     console.log("liff.init() done");
+  //     setLiffObject(liff);
+  //   } catch (error) {
+  //     console.log(`liff.init() failed: ${error}`);
+  //     if (!process.env.liffId) {
+  //       console.info(
+  //         "LIFF Starter: Please make sure that you provided `LIFF_ID` as an environmental variable."
+  //       );
+  //     }
+  //     setLiffError(error.toString());
+  //   }
+  // }, []);
 
   // Provide `liff` object and `liffError` object
   // to page component as property
-  pageProps.liff = liffObject;
-  pageProps.profile = liffProfile; // TEST: 別コンポーネントで持たせたい
+  // pageProps.liff = liffObject;
   // pageProps.liffError = liffError;
   return <Component {...pageProps} />;
 }
--- src/nextjs/pages/report.js
+++ src/nextjs/pages/report.js
@@ -1,63 +1,28 @@
-import { useEffect, useState } from "react";
-import { Image } from "next";
+import { useEffect } from "react";
 
 export default function Report(props) {
-  // const [mounted, setMounted] = useState(false);
-  // const [profile, setProfile] = useState({
-  //   userId: "",
-  //   displayName: "",
-  //   pictureUrl: "",
-  //   statusMessage: "",
-  // });
+  useEffect(async () => {
+    const liff = (await import("@line/liff")).default;
+    await liff.init({
+      liffId: process.env.LIFF_ID_REPORT_APP,
+    });
+    await liff
+    .sendMessages([
+      {
+        type: "text",
+        text: JSON.stringify(profile),
+      },
+    ])
+    .then(() => {
+      liff.closeWindow();
+    })
+    .catch((error) => {
+      alert(JSON.stringify(error));
+    });
+  });
 
-  // useEffect(() => {
-  //   setMounted(true);
-  // }, []);
-
-  // useEffect(async () => {
-  //   // const liff = (await import("@line/liff")).default;
-  //   // console.log({ liff }, liff.isLoggedIn());
-  //   const waitUntil = async (conditionCallback, intervalMiliSecond = 100) => {
-  //     return new Promise((resolve) => {
-  //       if (conditionCallback()) {
-  //         return resolve();
-  //       }
-  //       const iid = setInterval(() => {
-  //         if (conditionCallback()) {
-  //           console.log({props})
-  //           clearInterval(iid);
-  //           return resolve();
-  //         }
-  //       }, intervalMiliSecond);
-  //     });
-  //   };
-  //   console.log("useEffect", props)
-  //   await waitUntil(() => props.liff && props.liff.isLoggedIn());
-  //   // await liff.ready;
-  //   const profile = await props.liff.getProfile();
-  //   setProfile(profile);
-  //   console.log({ profile }); // ここは取れているが、それ以前でエラー
-  //   // Error: Minified React error #130; visit https://reactjs.org/docs/error-decoder.html?invariant=130&args[]=undefined&args[]= for the full message or use the non-minified dev environment for full errors and additional helpful warnings.
-  // }, [profile.userId]);
-
-  const { liff, profile } = props;
-  console.log(liff, profile);
-
+  // TODO: APIのエンドポイントとして使いたい時 Next.jsではどうする?
   return (
-    <section>
-      <h1>Profile</h1>
-      <div>
-        てすと
-        {/* {profile.pictureUrl && (
-          <Image
-            src={profile.pictureUrl}
-            alt={profile.displayName}
-            width={500}
-            height={500}
-          />
-        )}
-        <div>Name: {profile.displayName}</div> */}
-      </div>
-    </section>
+    <></>
   );
 }
