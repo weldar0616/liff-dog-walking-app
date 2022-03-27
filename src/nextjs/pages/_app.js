93ae3907a3974bb1b88622cccba84a9f2abe84a9
ogata <ogata@beaglee.com>
2022-03-27 15:20:35 +0900

    liffSDK試行

--- src/nextjs/pages/_app.js
+++ src/nextjs/pages/_app.js
@@ -2,38 +2,38 @@ import "../styles/globals.css";
 import { useState, useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
-  const [liffObject, setLiffObject] = useState(null);
-  const [liffError, setLiffError] = useState(null);
+  // const [liffObject, setLiffObject] = useState(null);
+  // const [liffError, setLiffError] = useState(null);
 
   // Execute liff.init() when the app is initialized
-  useEffect(() => {
+  useEffect(async () => {
     // to avoid `window is not defined` error
-    import("@line/liff").then((liff) => {
+    const liff = (await import("@line/liff")).default;
+    try {
       console.log("start liff.init()...");
       alert("start liff.init()...");
-      liff
-        .init({ liffId: process.env.LIFF_ID })
-        .then(() => {
-          console.log("liff.init() done");
-          alert("liff.init() done");
-          setLiffObject(liff);
-        })
-        .catch((error) => {
-          console.log(`liff.init() failed: ${error}`);
-          if (!process.env.liffId) {
-            console.info(
-              "LIFF Starter: Please make sure that you provided `LIFF_ID` as an environmental variable."
-            );
-          }
-          setLiffError(error.toString());
-        });
-    });
+      await liff.init({ liffId: process.env.LIFF_ID });
+      console.log("liff.init() done");
+      alert("liff.init() done");
+      // setLiffObject(liff);
+    } catch (error) {
+      console.log(`liff.init() failed: ${error}`);
+      if (!process.env.liffId) {
+        console.info(
+          "LIFF Starter: Please make sure that you provided `LIFF_ID` as an environmental variable."
+        );
+      }
+      // setLiffError(error.toString());
+    }
+    if (!liff.isLoggedIn()) {
+      liff.login();
+    }
   }, []);
 
   // Provide `liff` object and `liffError` object
   // to page component as property
-  pageProps.liff = liffObject;
-  pageProps.liffError = liffError;
+  // pageProps.liff = liffObject;
+  // pageProps.liffError = liffError;
   return <Component {...pageProps} />;
 }
 
--- src/nextjs/pages/report.js
+++ src/nextjs/pages/report.js
@@ -1,64 +1,31 @@
-import { Button } from "@mui/material";
+import { useEffect, useState } from "react";
+import { Head, Image } from "next";
 
 export default function Report(props) {
-  console.log({ props });
-  const liff = props.liff;
-  const liffError = props.liffError;
+  const [profile, setProfile] = useState({});
 
-  const XXX = (props) => {
-    alert("sendMessages:" + JSON.stringify(props.liff));
-    const liff = props.liff;
-    try {
-      liff.getProfile().then((profile) => {
-        console.log({ profile });
-        liff
-          .sendMessages([
-            {
-              type: "text",
-              text: "Hello:" + JSONs.stringify(profile),
-            },
-          ])
-          .then((res) => {
-            alert("closeWindow:" + JSON.stringify(res));
-            liff.closeWindow();
-          })
-          .catch((err) => {
-            alert("reject send messages: " + JSON.stringify(err));
-            liff.closeWindow();
-          });
-      });
-    } catch (err) {
-      return <span>{err}</span>;
-    }
-    return <span>Loading...</span>;
-  };
+  useEffect(async () => {
+    const liff = (await import("@line/liff")).default;
+    console.log({ liff });
+    await liff.ready;
+    const profile = await liff.getProfile();
+    setProfile(profile);
+  }, [profile.userId]);
 
-  console.log({ liff });
-  const content = (
-    <div>
-      {liffError ? (
-        liffError
-      ) : !liff ? (
-        "liff is undefined..."
-      ) : (
-        <XXX liff={liff} />
-      )}
-    </div>
+  return (
+    <section>
+      <h1>Profile</h1>
+      <div>
+        {profile.pictureUrl && (
+          <Image
+            src={profile.pictureUrl}
+            alt={profile.displayName}
+            width={500}
+            height={500}
+          />
+        )}
+        <div>Name: {profile.displayName}</div>
+      </div>
+    </section>
   );
-
-  return content;
-  // const onClick = () => {
-  //   if (props.liff && !props.liff.isLoggedIn()) {
-  //     props.liff.login();
-  //   }
-  // };
-  // return (
-  //   <>
-  //     <div>
-  //       <Button variant="outlined" onClick={onClick}>
-  //         Login
-  //       </Button>
-  //     </div>
-  //   </>
-  // );
 }
