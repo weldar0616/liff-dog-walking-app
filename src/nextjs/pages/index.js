29cb6afecdac9a49b61123070338698720ddf737
ogata <ogata@beaglee.com>
2022-04-27 21:49:46 +0900

    #18 Remove unused code.

--- src/nextjs/pages/index.js
+++ src/nextjs/pages/index.js
@@ -1,31 +1,4 @@
-import { useEffect, useState } from "react";
 import Head from "next/head";
-
-function Content(props) {
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(async () => {
-    const liff = (await import("@line/liff")).default;
-    await liff.ready;
-    setMounted(true);
-  }, []);
-
-  return (
-    mounted && (
-      <>
-        <h1 className="home__title">
-          Welcome to <br />
-          <a
-            className="home__title__link"
-            href="https://developers.line.biz/en/docs/liff/overview/"
-          >
-            LIFF Starter!
-          </a>
-        </h1>
-      </>
-    )
-  );
-}
 
 export default function Home(props) {
   return (
@@ -33,9 +6,7 @@ export default function Home(props) {
       <Head>
         <title>散歩当番</title>
       </Head>
-      <div className="home">
-        <Content liff={props.liff} liffError={props.liffError} />
-      </div>
+      <div className="home">散歩当番</div>
     </div>
   );
 }
