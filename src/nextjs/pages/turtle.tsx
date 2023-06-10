79922c96b31aa65cbab6ba0ee8f075d208daedad
weldar0616 <mitsuhiro.ogata@uzabase.com>
2023-06-10 13:42:38 +0900

    レポート形式を亀の餌やり用に

--- src/nextjs/pages/turtle.tsx
+++ src/nextjs/pages/turtle.tsx
@@ -1,16 +1,14 @@
 import { NextPage } from "next";
-import ReportText from "../components/reportText";
 import { useReport } from "../hooks/useReport";
 import {
   formatTime,
-  periodLabel,
 } from "../libs/report";
 import { createReport } from "../types/report";
 
 const createReport: createReport = async (userName) => {
     const date = new Date();
     return (
-      `${periodLabel()} ${formatTime(date.getHours())}:${formatTime(
+      `${formatTime(date.getHours())}:${formatTime(
         date.getMinutes()
       )}\n` +
       `${userName}さんが亀の餌やりをしました。`
@@ -20,7 +18,7 @@ const createReport: createReport = async (userName) => {
   const Report: NextPage = () => {
     useReport(process.env.LIFF_ID_TURTLE_APP, createReport);
   
-    return <ReportText periodLabel={periodLabel()} />;
+    return <div>亀の餌やりを報告中...</div>
   };
   
   export default Report;
